4e2cedc9c4d0bae9ce59f30f4acffb185b3c398a
wangzida <17355477009@163.com>
2023-11-14 01:57:10 +0800

    增加图片传输最大大小

--- src/main/java/com/example/jadows/config/WebSocketConfig.java
+++ src/main/java/com/example/jadows/config/WebSocketConfig.java
@@ -2,10 +2,12 @@ package com.example.jadows.config;
 
 import com.example.jadows.handler.WebSocketHandler;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 @EnableWebSocket
 @Configuration
@@ -15,9 +17,18 @@ public class WebSocketConfig implements WebSocketConfigurer {
 	private final WebSocketHandler webSocketHandler;
 
 
+	private final Integer MAX_FILE_SIZE = 1024 * 1024 * 20;
+
 	@Override
 	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
 		registry.addHandler(webSocketHandler, "/mini/socket").setAllowedOrigins("*");
+	}
+
+	@Bean
+	public ServletServerContainerFactoryBean createServletServerContainerFactoryBean() {
+		ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+		container.setMaxTextMessageBufferSize(MAX_FILE_SIZE);
+		return container;
 	}
 
 }
